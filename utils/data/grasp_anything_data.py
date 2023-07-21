f3bb8dce12cb011fee5acf89eba5ee689bfaa388
anavuongdin <timeskipfor8months@gmail.com>
2023-07-21 10:13:31 +0700

    Fix typo

--- utils/data/grasp_anything_data.py
+++ utils/data/grasp_anything_data.py
@@ -30,7 +30,7 @@ class GraspAnythingDataset(GraspDatasetBase):
         with open(os.path.join(file_path, 'idxs.obj'), 'rb') as f:
             idxs = pickle.load(f)
 
-        self.grasp_files = list(filter(lambda x: x.splot('/')[-1].split('_')[0] in idxs, self.grasp_files))
+        self.grasp_files = list(filter(lambda x: x.split('/')[-1].split('_')[0] in idxs, self.grasp_files))
         self.grasp_files.sort()
         self.prompt_files.sort()
         self.rgb_files.sort()
